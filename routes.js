b294404953284b8b4fa89aa0316ecbf49755c022
Cathy <139692613+CatherinNaurah@users.noreply.github.com>
2025-05-29 10:50:24 +0700

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,15 @@
+import RegisterPage from '../pages/auth/register/page';
+import LoginPage from '../pages/auth/login/page';
+import HomePage from '../pages/home/page';
+import InfoPage from '../pages/info/page';
+import { checkAuthenticatedRoute, checkUnauthenticatedRouteOnly } from '../utils/auth';
+
+export const routes = {
+  '/login': () => checkUnauthenticatedRouteOnly(new LoginPage()),
+  '/register': () => checkUnauthenticatedRouteOnly(new RegisterPage()),
+  '/nidracare': () => checkUnauthenticatedRouteOnly(new Nidracare()),
+  '/info': () => checkUnauthenticatedRouteOnly(new InfoPage()),
+  '/home': () => checkAuthenticatedRoute(new HomePage()),
+
+  '/': () => checkUnauthenticatedRouteOnly(new HomePage()),
+};
